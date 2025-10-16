45183f72721730a5cfff28868f398cc48a89cff6
KlevisonF <silvaklevison11@gmail.com>
2025-10-16 11:33:10 -0300

    adiciona scripts `lint:chec` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-function Home(){
-    return (
-        <div>
-            <div>
-                <h1>Eu te amo meu amooor </h1>
-                <h1>Eu te amo meu amooor </h1>
-            </div>
-            
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Eu te amo meu amooor </h1>
+        <h1>Eu te amo meu amooor</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
-
-
