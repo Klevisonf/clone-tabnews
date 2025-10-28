48512c60001b1c68b9cf8369a2221e104edaad5f
KlevisonF <silvaklevison11@gmail.com>
2025-10-28 09:43:47 -0300

    Adicionando Test Runner

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ function Home() {
   return (
     <div>
       <div>
-        <h1>Eu te amo meu amooor </h1>
-        <h1>Eu tea amo meu amooor</h1>
+        <h1>Primeiro </h1>
+        <h1></h1>
       </div>
     </div>
   );
