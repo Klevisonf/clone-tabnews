431b1bf95dd2f3c634aa111f585a1b45c6b22c1b
KlevisonF <silvaklevison11@gmail.com>
2025-11-10 12:39:15 -0300

    add `database.js` and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   res.status(200).json({ chave: "Alunos do curso.dev estao demais" });
 }
 
